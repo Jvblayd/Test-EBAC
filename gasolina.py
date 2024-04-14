58393c458932116578d5ef366834d3ebbbe5b3ec
jvblayd <joaovitormdep@gmail.com>
2024-04-14 00:02:30 +0000

    Adicionando arquivos

---
+++ gasolina.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import seaborn as sns
+
+gasolina_df = pd.read_csv('gasolina.csv')
+gasolina_graph = sns.lineplot(gasolina_df, x='dia', y='venda')
+gasolina_graph.set(title='Vendas Diárias de Gasolina', xlabel='Dias', ylabel='Vendas')
+
+fig = gasolina_graph.get_figure()
+fig.savefig('gasolina.png')
