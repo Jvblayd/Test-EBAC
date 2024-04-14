af06733910be0094677fb71e6638733c4b4280b0
jvblayd <joaovitormdep@gmail.com>
2024-04-14 00:54:10 +0000

    Alterando arquivos gasolina.png e gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 gasolina_df = pd.read_csv('gasolina.csv')
 gasolina_graph = sns.lineplot(gasolina_df, x='dia', y='venda')
-gasolina_graph.set(title='Vendas Diárias de Gasolina', xlabel='Dias', ylabel='Vendas')
+gasolina_graph.set(title='Vendas Diárias de Gasolina', xlabel='Dias', ylabel='Vendas (R$)')
 
 fig = gasolina_graph.get_figure()
 fig.savefig('gasolina.png')
